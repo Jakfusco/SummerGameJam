87efae28b0ecab50848cce1d54b08e0f9d418952
Jake Fusco <darthmassa@gmail.com>
2020-07-18 08:23:08 -0700

    Add Project

---
+++ UAT Summer Jam/Assets/Scripts/Mouse_Look.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Look : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float mouseSensitiviy = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitiviy * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitiviy * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); 
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}

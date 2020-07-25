a0527124a4caa3a9f935b27169c3c3281a7e1580
Jake Fusco <darthmassa@gmail.com>
2020-07-24 19:41:01 -0700

    Added UI Functionaity (somewhat)

---
+++ UAT Summer Jam/Assets/Scripts/PartyScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartyScore : MonoBehaviour
+{
+    public GameObject designerScript;
+    public DesignerChanges designerChanges;
+    public Text partyMeter;
+    // Update is called once per frame
+    private void Start()
+    {
+        designerChanges = designerScript.GetComponent<DesignerChanges>();
+    }
+    void Update()
+    {
+        partyMeter.text = ("Party Meter: " + designerChanges.partyMeter.ToString());
+    }
+
+}
+
---
+++ UAT Summer Jam/Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public GameObject designerScript;
+    public DesignerChanges designerChanges;
+    public Text score;
+    // Update is called once per frame
+    private void Start()
+    {
+        designerChanges = designerScript.GetComponent<DesignerChanges>();
+    }
+    void Update()
+    {
+        score.text = ("Score: " + designerChanges.score.ToString());
+    }
+
+}
+

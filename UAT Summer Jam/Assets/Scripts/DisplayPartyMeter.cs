10b68a97da1c243b49f8ebd2204ebcd550f8f4d8
Jake Fusco <darthmassa@gmail.com>
2020-07-25 21:31:50 -0700

    DisplayScore added and began work on DisplayPartyMeter. PLease god dont break the build.

---
+++ UAT Summer Jam/Assets/Scripts/DisplayPartyMeter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayPartyMeter : MonoBehaviour
+{
+    public GameObject designerScript;
+    public DesignerChanges designerChanges;
+    public float displayPartyMeter;
+    public int max;
+    public int current;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayPartyMeter = designerChanges.partyMeter;
+    }
+}
---
+++ UAT Summer Jam/Assets/Scripts/DisplayScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour
+
+{
+    public GameObject designerScript;
+    public DesignerChanges designerChanges;
+    public Text displayScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        designerChanges = designerScript.GetComponent<DesignerChanges>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayScore.text = ("Score: " + designerChanges.score);
+    }
+}
